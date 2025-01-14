dcee74ad5364398921920ecddbbe61de656feed2
LapPavel <wizd@tut.by>
2025-01-14 16:47:11 +0300

    feat: add solution for 3rd task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folder, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+
+  files
+    .filter((file) => file.isFile())
+    .forEach((file) => {
+      const filePath = path.join(folder, file.name);
+      fs.stat(filePath, (err, stats) => {
+        if (err) throw err;
+        const { name, ext } = path.parse(filePath);
+        console.log(
+          `${name} - ${ext.slice(1)} - ${(stats.size / 1024).toFixed(2)} kb`,
+        );
+      });
+    });
+});
